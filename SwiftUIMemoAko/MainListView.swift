ab606ea3e1d6bbc4ff2ab40a97692d12fa971a7a
sunminhong <akoda5555@gmail.com>
2024-02-19 15:50:02 +0900

    Edit Directory

---
+++ SwiftUIMemoAko/MainListView.swift
@@ -0,0 +1,18 @@
+//
+//  MainListView.swift
+//  SwiftUIMemoAko
+//
+//  Created by SunMin Hong on 2/19/24.
+//
+
+import SwiftUI
+
+struct MainListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainListView()
+}
