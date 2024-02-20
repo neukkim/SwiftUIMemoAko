be2380bdcb0976fc3ced72c16a42af22788865c4
sunminhong <akoda5555@gmail.com>
2024-02-20 22:49:32 +0900

    Add Memo, MemoStore class

--- SwiftUIMemoAko/Memory/Memo.swift
+++ SwiftUIMemoAko/Memory/Memo.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+import SwiftUI
+
+class Memo: Identifiable, ObservableObject {
+  //프로토콜
+  // Identifiable: 데이터를 리스트에 쉽게 바인딩
+  // ObservableObject: 메모를 편집, 뷰를 자동으로 업데이트
+  let id: UUID
+  @Published var content: String
+  //@Published 특성을 추가
+  //새로운 값을 추가 하면 바인딩 되어 있는 UI가 자동으로 업데이트
+  let insertDate: Date
+  
+  init(content: String, insertDate: Date = Date.now) {
+    id = UUID()
+    self.content = content
+    self.insertDate = insertDate
+  }
+}
