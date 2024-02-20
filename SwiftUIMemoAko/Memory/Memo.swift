f263930d7289e7c392273b9226c85ab8685f27ee
sunminhong <akoda5555@gmail.com>
2024-02-20 22:48:12 +0900

    Add  Memo, MemoStore class

---
+++ SwiftUIMemoAko/Memory/Memo.swift
@@ -0,0 +1,8 @@
+//
+//  Memo.swift
+//  SwiftUIMemoAko
+//
+//  Created by SunMin Hong on 2/20/24.
+//
+
+import Foundation
---
+++ SwiftUIMemoAko/Memory/MemoStore.swift
@@ -0,0 +1,8 @@
+//
+//  MemoStore.swift
+//  SwiftUIMemoAko
+//
+//  Created by SunMin Hong on 2/20/24.
+//
+
+import Foundation
