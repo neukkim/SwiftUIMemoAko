c926f39ecbd04c1ef04cab4c8c440ccb18010893
sunminhong <akoda5555@gmail.com>
2024-02-22 00:59:28 +0900

    memo list 구현

--- SwiftUIMemoAko/MainListView.swift
+++ SwiftUIMemoAko/MainListView.swift
@@ -8,11 +8,33 @@
 import SwiftUI
 
 struct MainListView: View {
+  @EnvironmentObject var store: MemoStore
+  // @EnvironmentObject 하나의 데이터를 여려뷰에서 공유
+  
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+      //클로저
+      NavigationView {
+        List(store.list) { memo in
+          // .leading: 왼쪽 정렬
+          VStack(alignment: .leading) {
+            Text(memo.content)
+              .font(.body)
+              .lineLimit(1)
+            
+            Text(memo.insertDate, style: .date)
+              .font(.caption)
+              .foregroundStyle(.secondary)
+          }
+        }
+        .listStyle(.plain)
+        .navigationTitle("내 메모")
+        //모디파이어: 특별한 메소드
+        //필요한 속성 변경, 조작
+      }
     }
 }
 
 #Preview {
     MainListView()
+    .environmentObject(MemoStore())
 }
--- SwiftUIMemoAko/SwiftUIMemoAkoApp.swift
+++ SwiftUIMemoAko/SwiftUIMemoAkoApp.swift
@@ -9,6 +9,9 @@ import SwiftUI
 
 @main
 struct SwiftUIMemoAkoApp: App {
+  //메모 저장소 커스텀 데이터, 속성으로 저장
+  @StateObject var store = MemoStore()
+  
     let persistenceController = PersistenceController.shared
 
     var body: some Scene {
@@ -19,6 +22,7 @@ struct SwiftUIMemoAkoApp: App {
           //변경
           MainListView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
+                .environmentObject(store)
         }
     }
 }
